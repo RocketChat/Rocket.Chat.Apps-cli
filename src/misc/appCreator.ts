a594e1427a223deb172f1aa30133fcac2012d451
Manuel Bachl <m.bachl@finndrop.de>
2018-06-27 00:45:01 +0200

    Add additional files to .gitignore

--- src/misc/appCreator.ts
+++ src/misc/appCreator.ts
@@ -150,6 +150,29 @@ node_modules/
 
 # rc-apps package output
 dist/
+
+# JetBrains IDEs
+out/
+.idea/
+.idea_modules/
+
+# macOS
+.DS_Store
+.AppleDouble
+.LSOverride
+._*
+.DocumentRevisions-V100
+.fseventsd
+.Spotlight-V100
+.TemporaryItems
+.Trashes
+.VolumeIcon.icns
+.com.apple.timemachine.donotpresent
+.AppleDB
+.AppleDesktop
+Network Trash Folder
+Temporary Items
+.apdisk
 `;
 
         fs.writeFileSync(this.fd.mergeWithFolder('.gitignore'), toWrite, 'utf8');
