a5e68060f8a21f82673d2cb476deb147f86e8918
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2018-12-04 20:07:36 -0200

    Update appCreator.ts

--- src/misc/appCreator.ts
+++ src/misc/appCreator.ts
@@ -133,7 +133,7 @@ Here are some links to examples and documentation:
         const toWrite =
 `{
     "devDependencies": {
-        "@rocket.chat/apps-engine": "^1.3.1",
+        "@rocket.chat/apps-engine": "^1.3.2",
         "tslint": "^5.10.0",
         "typescript": "^2.9.1"
     }
