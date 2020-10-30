8257203ad82a9286254e77f65d97021fb275b79d
Douglas Gubert <douglas.gubert@gmail.com>
2020-10-30 19:20:56 -0300

    Update default version of packages to latest

--- src/templates/app/packageJsonTemplate.ts
+++ src/templates/app/packageJsonTemplate.ts
@@ -1,10 +1,10 @@
 export const packageJsonTemplate = (): string => {
     return `{
     "devDependencies": {
-        "@rocket.chat/apps-engine": "^1.12.0",
-        "@types/node": "^10.12.14",
+        "@rocket.chat/apps-engine": "^1.18.0",
+        "@types/node": ""14.14.6,
         "tslint": "^5.10.0",
-        "typescript": "^2.9.1"
+        "typescript": "^4.0.5"
     }
 }`;
 };
