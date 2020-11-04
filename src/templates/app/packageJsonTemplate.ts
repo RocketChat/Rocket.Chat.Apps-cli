80c24fee9a427e6a758c3586edcf8922667d8f52
Bradley Hilton <graywolf336@craftyn.com>
2020-11-04 16:50:43 -0600

    Fix typo in the json of the packageJsonTemplate

--- src/templates/app/packageJsonTemplate.ts
+++ src/templates/app/packageJsonTemplate.ts
@@ -2,7 +2,7 @@ export const packageJsonTemplate = (): string => {
     return `{
     "devDependencies": {
         "@rocket.chat/apps-engine": "^1.18.0",
-        "@types/node": ""14.14.6,
+        "@types/node": "14.14.6",
         "tslint": "^5.10.0",
         "typescript": "^4.0.5"
     }
