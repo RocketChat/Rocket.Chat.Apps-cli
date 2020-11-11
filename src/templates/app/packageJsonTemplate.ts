fe0143428e27377c61b0778fb2b6b0428f69137a
Douglas Gubert <d-gubert@users.noreply.github.com>
2020-11-11 16:40:11 -0300

    Update Apps-Engine version to latest

--- src/templates/app/packageJsonTemplate.ts
+++ src/templates/app/packageJsonTemplate.ts
@@ -1,7 +1,7 @@
 export const packageJsonTemplate = (): string => {
     return `{
     "devDependencies": {
-        "@rocket.chat/apps-engine": "^1.18.0",
+        "@rocket.chat/apps-engine": "^1.19.0",
         "@types/node": "14.14.6",
         "tslint": "^5.10.0",
         "typescript": "^4.0.5"
