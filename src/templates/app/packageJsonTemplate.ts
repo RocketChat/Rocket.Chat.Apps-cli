80177aa50415b34311c417fb65387e765ae0f2c5
gustrb <gustavoreisbauer@gmail.com>
2024-09-25 17:02:19 -0300

    chore: updating the package.json versions

--- src/templates/app/packageJsonTemplate.ts
+++ src/templates/app/packageJsonTemplate.ts
@@ -1,10 +1,14 @@
 export const packageJsonTemplate = (): string => {
     return `{
     "devDependencies": {
-        "@rocket.chat/apps-engine": "^1.19.0",
+        "@rocket.chat/apps-engine": "^1.45.0",
         "@types/node": "14.14.6",
         "tslint": "^5.10.0",
-        "typescript": "^4.0.5"
+        "typescript": "^5.6.2"
+    },
+    "dependencies": {
+      	"@rocket.chat/icons": "^0.38.0",
+		"@rocket.chat/ui-kit": "^0.36.1"
     }
 }`;
 };
