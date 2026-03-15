6e95a79a64a16b36212abd29d4e3c23d65f4c5f6
shubham <shubhamsrivastava12568@gmail.com>
2026-03-15 22:59:42 +0530

    fix(packager): normalize Windows path separators in zip entries

--- src/misc/appPackager.ts
+++ src/misc/appPackager.ts
@@ -59,7 +59,7 @@ export class AppPackager {
         zip.addBuffer(Buffer.from(JSON.stringify(AppPackager.PackagerInfo)), '.packagedby', { compress: true });
 
         for (const realPath of matches) {
-            const zipPath = path.relative(this.fd.folder, realPath);
+            const zipPath = path.relative(this.fd.folder, realPath).replace(/\\/g, '/');
             const fileStat = await fs.stat(realPath);
 
             const options: Partial<Yazl.Options> = {
